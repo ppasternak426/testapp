c9d381cd1f8048426e026ad4764cbcb510b8b7c5
Piotr Robert. Pasternak <piotr.pasternak1@pl.abb.com>
2021-10-19 14:34:12 +0200

    Initial

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,18 @@
+import {AdditionalText} from "./AdditionalText/AdditionalText"
+
+export function Footer() {
+  return (
+    <footer>
+      <div className='container'>
+        <a href='/#/' className='logo-font'>
+          conduit
+        </a>
+        <AdditionalText />
+        <span className='attribution'>
+          An interactive learning project from <a href='https://thinkster.io'>Thinkster</a>. Code &amp; design licensed
+          under MIT.
+        </span>
+      </div>
+    </footer>
+  );
+}
